65c1fb25ff9edf26af7fb98557f1534b22726208
code-xian <code_xian@163.com>
2024-03-29 18:19:15 +0800

    feat: 增加插件入参 limit，excludeDnsPrefetchUrl

--- script/index.js
+++ script/index.js
@@ -1,23 +1,28 @@
 import {parse} from "node-html-parser";
-import { glob } from "glob";
+import {glob} from "glob";
 import urlRegex from 'url-regex'
 import fs from 'fs'
 
-const vitePluginDnsPrefetch = () => {
-  const excludeDnsPrefetchUr = []
+const vitePluginDnsPrefetch = ({
+   limit = 20,
+   excludeDnsPrefetchUrl = []
+                               }) => {
   const urlPattern = /(https?:\/\/[^/]*)/i
   const urls = new Set()
 
   async function searchDomain() {
-    const files = await glob('dist/**/*.{html,css,js}', { ignore: 'node_modules/**' });
+    const files = await glob('dist/**/*.{html,css,js}', {ignore: 'node_modules/**'});
     for (const file of files) {
       const source = fs.readFileSync(file, 'utf-8')
       const matches = source.match(urlRegex({strict: true}))
       if (matches) {
         matches.forEach(url => {
           const match = url.match(urlPattern)
-          if (match && match[1] && !excludeDnsPrefetchUr.includes(match[1])) {
+          if (match && match[1] && !excludeDnsPrefetchUrl.includes(match[1])) {
             urls.add(match[1])
+            if (urls.size >= limit) {
+              return
+            }
           }
         })
       }
