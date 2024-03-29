f34936abb9135f95e57b75f86b150afb57de644b
code-xian <code_xian@163.com>
2024-03-29 17:21:38 +0800

    feat: dns-prefetch 功能提交

---
+++ script/index.js
@@ -0,0 +1,51 @@
+import {parse} from "node-html-parser";
+import { glob } from "glob";
+import urlRegex from 'url-regex'
+import fs from 'fs'
+
+const vitePluginDnsPrefetch = () => {
+  const excludeDnsPrefetchUr = []
+  const urlPattern = /(https?:\/\/[^/]*)/i
+  const urls = new Set()
+
+  async function searchDomain() {
+    const files = await glob('dist/**/*.{html,css,js}', { ignore: 'node_modules/**' });
+    for (const file of files) {
+      const source = fs.readFileSync(file, 'utf-8')
+      const matches = source.match(urlRegex({strict: true}))
+      if (matches) {
+        matches.forEach(url => {
+          const match = url.match(urlPattern)
+          if (match && match[1] && !excludeDnsPrefetchUr.includes(match[1])) {
+            urls.add(match[1])
+          }
+        })
+      }
+    }
+  }
+
+  async function insertLinks() {
+    const files = await glob('dist/**/*.html')
+    let links = '\n';
+    links = links += [...urls].map(url => `\t<link rel="dns-prefetch" href="${url}" />`).join('\n');
+    for (const file of files) {
+      const html = fs.readFileSync(file, 'utf-8')
+      const root = parse(html);
+      const head = root.querySelector('head')
+      head.insertAdjacentHTML('afterbegin', links);
+      fs.writeFileSync(file, root.toString())
+    }
+
+  }
+
+  return {
+    name: 'vitePluginDnsPrefetch',
+    async closeBundle() {
+      await searchDomain()
+      await insertLinks()
+    }
+  }
+}
+
+
+export default vitePluginDnsPrefetch
---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import {defineConfig} from 'vite';
+import path from 'path';
+import inspect from 'vite-plugin-inspect'
+
+export default defineConfig({
+  plugins: [
+    inspect(),
+  ],
+  build: {
+    rollupOptions: {
+      external: ['glob'],
+    },
+    lib: {
+      entry: path.resolve(__dirname, 'script/index.js'),
+      name: "vitePluginDnsPrefetch",
+      fileName: "main",
+      formats: ["es", "cjs"]
+    }
+  }
+})
